5798cb582ccefaada94e4e2d3f77c85222b0f765
Leon Liu <le0nlpersonalpython@gmail.com>
2025-03-31 13:06:11 +1300

    Commiting to save craetion of css and jjs files
    Implemented css files to design web pages
    Implemented js files to give functionality to website components

---
+++ js/popup.js
@@ -0,0 +1,12 @@
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+
+const day = document.querySelector(".calender-dates");
+
+const currdate = document
+    .querySelector(".calendar-current-date");
+
+const limit = 5;
+
+const
